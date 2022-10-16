a3e153efd2afbbb96e657ee194d1b213db4b37c7
chiragnc22 <chiragnc22@gmail.com>
2022-10-16 12:47:38 +0530

    form submission update

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const sections = document.querySelectorAll("section");
 
 let activeLink = 0;
 
+function showSnackbar() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 3000);
+}
+
 links.forEach((link, i) => {
   link.addEventListener("click", () => {
     if (activeLink != i) {
